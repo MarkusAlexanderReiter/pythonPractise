df5d4ca60dceaadd0a0aacb4606c11621fd31f1a
Markus <info@reiter-digital.at>
2025-10-26 21:02:14 +0100

    scaffolded menu options

--- Markdown_Notes/markdown-notes.py
+++ Markdown_Notes/markdown-notes.py
@@ -4,9 +4,32 @@ from pathlib import Path
 
 notes_dir = Path(__file__).parent/"notes"
 
+#shortcut functions...
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def invalid_input(): 
+    print('Invalid input! Please try again.')
+
 #returns default directory and creates it if needed
 def get_notes_directory(): 
     if not notes_dir.exists():
         notes_dir.mkdir()
         return notes_dir
+    
+def menu():
+    while True:
+        clear_screen
+        select = input('[1] Create new file\n[2] View/search\n[3] Settings\n[0] Exit\n\n> ')
+        if select == '1':
+            print('1')
+        elif select == '2':
+            print('2')
+        elif select == '3':
+            print('3')
+        elif select == '4':
+            break
+        else: invalid_input()
+
+    
     
